daadcba774c3361737d3698b035867e102071a9d
oscar <oscar.goodwin@gmail.com>
2021-11-13 08:42:08 +0800

    Create custom view class

--- app/src/main/java/com/example/custom_fan_controller/DialView.kt
+++ app/src/main/java/com/example/custom_fan_controller/DialView.kt
@@ -1,4 +1,42 @@
 package com.example.custom_fan_controller
 
-class DialView {
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.PointF
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.view.View
+
+
+private enum class FanSpeed(val label: Int) {
+    OFF(R.string.fan_off),
+    LOW(R.string.fan_low),
+    MEDIUM(R.string.fan_medium),
+    HIGH(R.string.fan_high)
+}
+
+private const val RADIUS_OFFSET_LABEL = 30
+private const val RADIUS_OFFSET_INDICATOR = -35
+
+//The @JvmOverloads annotation instructs the Kotlin compiler to generate overloads for this function that substitute default parameter values.
+class DialView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) { //Extend view class
+
+    private var radius = 0.0f                   // Radius of the circle.
+    private var fanSpeed = FanSpeed.OFF         // The active selection.
+    // position variable which will be used to draw label and indicator circle position
+    private val pointPosition: PointF = PointF(0.0f, 0.0f)
+
+    //initialize a Paint object with a handful of basic styles.
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create( "", Typeface.BOLD)
+    }
+
+
 }
